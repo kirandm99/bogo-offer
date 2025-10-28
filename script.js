4d1dd6bfd5e8751faea607d36abbee7197de929c
kiran <kirandm99@gmail.com>
2025-10-28 23:04:23 +0530

    update radio btn

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const boxes = document.querySelectorAll(".offer-box");
 const totalDisplay = document.getElementById("total");
 
 boxes.forEach((box) => {
+  const radio = box.querySelector("input[type='radio']");
+
   box.addEventListener("click", () => {
-    boxes.forEach((b) => b.classList.remove("active"));
+    boxes.forEach((b) => {
+      b.classList.remove("active");
+      b.querySelector("input[type='radio']").checked = false;
+    });
+
     box.classList.add("active");
+    radio.checked = true;
 
     const price = box.dataset.price;
     totalDisplay.textContent = `$${price}.00 USD`;
